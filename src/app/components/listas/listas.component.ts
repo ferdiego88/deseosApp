6828701392e4cf711b63d53b2e6f4fa2c8c048c5
Fernando Santillan <ferdiego88@gmail.com>
2021-02-27 19:49:48 -0500

    fsvterminada

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Lista } from 'src/app/models/lista.model';
 import { DeseosService } from 'src/app/providers/deseos.service';
+import { AlertController, IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-listas',
@@ -9,9 +10,11 @@ import { DeseosService } from 'src/app/providers/deseos.service';
   styleUrls: ['./listas.component.scss'],
 })
 export class ListasComponent   {
+  @ViewChild(IonList)lista: IonList;
   @Input() terminada = true;
   constructor(public deseosService: DeseosService,
-              private router: Router)
+              private router: Router,
+              public alertController: AlertController)
   {
   }
 
@@ -26,6 +29,44 @@ export class ListasComponent   {
 
   borrarLista(lista: Lista){
     this.deseosService.borrarLista(lista);
+  }
+
+  async editarLista(lista: Lista){
+    const alert = await this.alertController.create({
+      header: 'Modificar Lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          value: lista.titulo,
+          placeholder: 'Nombre de la Lista'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+            this.lista.closeSlidingItems();
+          }
+        },
+        {
+          text: 'Actualizar',
+          handler: (data ) => {
+            console.log(data);
+            if (data.titulo.lenght === 0) {
+              return;
+            }
+            // Guardo el nuevo nombre de la lista
+            lista.titulo = data.titulo;
+            this.deseosService.saveStorage();
+            this.lista.closeSlidingItems();
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
 }
